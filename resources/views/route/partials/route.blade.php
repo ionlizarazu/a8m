81221bbb8ad455c0c9bb8f7cab9d36f0714c8c7a
markelor <mendimarkel@gmail.com>
2018-02-02 20:37:37 +0100

    wikiloc

--- resources/views/route/partials/route.blade.php
+++ resources/views/route/partials/route.blade.php
@@ -1,3 +1,2 @@
-<div style="width: 100%; height: 800px;">
-    {!! Mapper::render() !!}
-</div>
+
+<iframe frameBorder="0" scrolling="no" src="https://es.wikiloc.com/wikiloc/spatialArtifacts.do?event=view&id=3948813&measures=on&title=on&near=on&images=on&maptype=S" width="100%" height="800"></iframe><div style="background-color:#fff;color:#777;font-size:11px;line-height:16px;">Powered by <a style="color:#06d;font-size:11px;line-height:16px;" target="_blank" href="https://es.wikiloc.com">Wikiloc</a></div>
