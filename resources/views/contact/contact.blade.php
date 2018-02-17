7415d27784100a828fcea26379e6e2521bf7b037
ionlizarazu <ionlizarazu@gmail.com>
2018-02-17 13:24:43 +0100

    mail change

--- resources/views/contact/contact.blade.php
+++ resources/views/contact/contact.blade.php
@@ -1,8 +1,6 @@
 @extends('index')
 @section('title','Jarri gurekin harremanetan')
-@section('description','Lasterketaren inguruan dituzun zalantza guztiak argitzeko aukera daukazu. Horretarako bete formularioa edota bidali mezu elektroniko bat antzuolako8miliak@gmail.com helbidera.')
-@lang('contact.description')
-@endsection
+@section('description','Lasterketaren inguruan dituzun zalantza guztiak argitzeko aukera daukazu. Horretarako bete formularioa edota bidali mezu elektroniko bat antzuolako8m@gmail.com helbidera.')
 @section('keywords', 'Antzuola kontaktua','mendi lasterketa kontaktua','a8m kontaktua','8 miliak kontaktua','zortzi miliak kontaktua','antzuolako 8 miliak kontaktua','antzuolako zortzi miliak kontaktua','antzuola lasterketa kontaktua','antzuola mendi lasterketa kontaktua','kontaktua')
 @section('canonical','http://www.a8m.kodekoop.eus/kontaktua')
 @section('header')
