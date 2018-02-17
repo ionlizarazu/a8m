ea884ac382f4747d03d31149b24342007455cbab
ionlizarazu <ionlizarazu@gmail.com>
2018-02-17 18:50:37 +0100

    about text

--- resources/views/about/partials/about.blade.php
+++ resources/views/about/partials/about.blade.php
@@ -10,9 +10,18 @@
     <!-- content -->
     <div class="about-content">
         <h2>Lasterketa Herrikoia</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat eu nibh ultricies semper. Vivamus porta, felis vitae facilisis sodales, felis est iaculis orci, et ornare sem mauris ut turpis. Pellentesque vitae tortor nec tellus hendrerit aliquam. Donec condimentum leo eu ull pellentesque urna rhoncus.</p>
-        <p>elis est iaculis orci, et ornare sem mauris ut turpis. Pellentesque vitae tortor nec tellus hendrerit aliquam. Donec condimentum leo eu ullamcorper scelerisque pellentes rhoncus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat eu nibh ultricies semper. Vivamus porta, felis vitae facilisis sodales, felis est iaculis orci, et ornare sem mauris ut turpis. Pellentesque vitae tortor nec tellus hendrerit aliquam. Donec condimentum leo eu ull pellentesque urna rhoncus.</p>
-        <p>elis est iaculis orci, et ornare sem mauris ut turpis. Pellentesque vitae tortor nec tellus hendrerit aliquam. Donec condimentum leo eu ullamcorper scelerisque pellentes rhoncus.</p>
+        <p>Antzuolako 8 miliak mendi lasterketa 2013ko Otsailean ospatu zen lehen aldiz. Orduztik honaino nabarmendu daitekeen ezaugarrietako bat lasterketaren herrikoitasuna da. Herritar ugari korrika irteten den arren, atzetik dagoen lanean ere makina bat jendek parte hartzen du. Antolakuntza, araudi eta izen emateetan talde txiki bat ibili ohi den arren, lasterketa aurreko hilabeteetan bide garbitze lanetan giro politean elkartzen da jendea. Zer esanik ez egunean bertan lasterketako bidegurutze bakoitzean petoa jantzi eta egun batez bada ere antolakuntzako kide izaten den jende mordoxka. Lan guzti honek herritarren bultzada izateak eman dio izana lasterketari.</p><p>
+Antolakuntzatik gaur egungo egoera ekonomikoarekin bat datorren lasterketa bat antolatzeko saiakera egin da, besteak beste, aurretiko izen-ematea 8 eurotan jartzea erabaki da, eta egunean bertan 15 euro.</p>
+<p><h3>Goizeko egitaraua:</h3>
+<ul>
+<li>10:30etan: A8M Helduen Mendi Lasterketako Irteera. </li>
+<li>11:00etan: A8M Minimotoen Lasterketa (1-4 urte artekoak).</li>
+<li>Ondoren: Bi frogen ostean, hamaiketakoa egongo da parte-hartzaile guztientzat.</li>
+<li>13:00etan: Sari banaketa.</li>
+</ul>
+</p><p>
+<h3>Ibilbidea:</h3> Proba, Antzuolan hasi (244m) eta 7. kilometroan Irimoko tontorrera helduko da, lasterketako puntu altuenera alegia (876m). Ondoren, puntaz punta, Trekutz-era jaitsi eta azken malda gogor bat igo ostean Antzuolarako azken jaitsiera egingo da. Lasterketa, 13,83 kilometro luze da eta 836m-ko desnibel positiboa izango du.
+</p>
     </div>
     <!-- content -->
     @include('footer.partners') 
