165caad3eeeaed305a4306fdb79a01c79e40ec55
markelor <mendimarkel@gmail.com>
2018-02-22 16:27:20 +0100

    header sintax

--- resources/views/header/header.blade.php
+++ resources/views/header/header.blade.php
@@ -16,17 +16,17 @@
           <div class="main-menu">
             <ul>
               <li class="{{ Request::is('/') ? "active" : "" }}" onclick="gtag('event', 'goiburukoa',{'event_category': 'aldaketa','event_label': 'Hasiera'});"><a href="{{url('/')}}">Hasiera</a></li>
-              <li class="{{ Request::is('izen-ematea') ? "active" : "" }}"" onclick="gtag('event', 'goiburukoa',{'event_category': 'aldaketa','event_label': 'izen-ematea'});"><a href="{{url('/izen-ematea')}}">Izen-ematea</a></li>
-              <li class="{{ Request::is('ibilbidea') ? "active" : "" }}"" onclick="gtag('event', 'goiburukoa',{'event_category': 'aldaketa','event_label': 'ibilbidea'});"><a href="{{url('/ibilbidea')}}">Ibilbidea</a></li>
+              <li class="{{ Request::is('izen-ematea') ? "active" : "" }}" onclick="gtag('event', 'goiburukoa',{'event_category': 'aldaketa','event_label': 'izen-ematea'});"><a href="{{url('/izen-ematea')}}">Izen-ematea</a></li>
+              <li class="{{ Request::is('ibilbidea') ? "active" : "" }}" onclick="gtag('event', 'goiburukoa',{'event_category': 'aldaketa','event_label': 'ibilbidea'});"><a href="{{url('/ibilbidea')}}">Ibilbidea</a></li>
               <div class="dropdown">
               <li class="dropdown-toggle" data-toggle="dropdown" onclick="gtag('event', 'goiburukoa',{'event_category': 'aldaketa','event_label': 'galeriak'});"><a href="">Galeriak</a></li>
               <ul id="galeriak" class="dropdown-menu">
-                <li class="{{ Request::is('galeria/2017') ? "active" : "" }}"" onclick="gtag('event', 'goiburukoa',{'event_category': 'aldaketa','event_label': '2017'});"><a href="{{url('/galeria/2017')}}">2017</a></li>
-                <li class="{{ Request::is('galeria/2018') ? "active" : "" }}"" onclick="gtag('event', 'goiburukoa',{'event_category': 'aldaketa','event_label': '2018'});"><a href="{{url('/galeria/2018')}}">2018</a></li>
+                <li class="{{ Request::is('galeria/2017') ? "active" : "" }}" onclick="gtag('event', 'goiburukoa',{'event_category': 'aldaketa','event_label': '2017'});"><a href="{{url('/galeria/2017')}}">2017</a></li>
+                <li class="{{ Request::is('galeria/2018') ? "active" : "" }}" onclick="gtag('event', 'goiburukoa',{'event_category': 'aldaketa','event_label': '2018'});"><a href="{{url('/galeria/2018')}}">2018</a></li>
               </ul>
               </div>
-              <li class="{{ Request::is('sailkapena') ? "active" : "" }}"" onclick="gtag('event', 'goiburukoa',{'event_category': 'aldaketa','event_label': 'sailkapena'});"><a href="{{url('/sailkapena')}}">Sailkapena</a></li>
-              <li class="{{ Request::is('kontaktua') ? "active" : "" }}"" onclick="gtag('event', 'goiburukoa',{'event_category': 'aldaketa','event_label': 'kontaktua'});"><a href="{{url('/kontaktua')}}">Kontaktua</a></li>
+              <li class="{{ Request::is('sailkapena') ? "active" : "" }}" onclick="gtag('event', 'goiburukoa',{'event_category': 'aldaketa','event_label': 'sailkapena'});"><a href="{{url('/sailkapena')}}">Sailkapena</a></li>
+              <li class="{{ Request::is('kontaktua') ? "active" : "" }}" onclick="gtag('event', 'goiburukoa',{'event_category': 'aldaketa','event_label': 'kontaktua'});"><a href="{{url('/kontaktua')}}">Kontaktua</a></li>
             </ul>
           </div>
         </div>
